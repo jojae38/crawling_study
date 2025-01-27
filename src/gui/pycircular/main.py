7705f8f7611a550d2114353b97748ce4717e9aa8
jojae <jojae38@naver.com>
2025-01-27 15:43:34 +0900

    수정정

--- src/gui/pycircular/main.py
+++ src/gui/pycircular/main.py
@@ -14,8 +14,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFo
 from PySide6.QtWidgets import *
 
 # GUI FILE
-from gui.pycircular.ui_splash_screen import Ui_SplashScreen
-from gui.pycircular.ui_main import Ui_MainWindow
+from ui_splash_screen import Ui_SplashScreen
+from ui_main import Ui_MainWindow
 
 # GLOBALS
 counter = 0
